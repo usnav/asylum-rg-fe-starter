bbbd1417d52e3888d02dbb0ef42fe431df58b61b
Usna Rafiqzadah <usna.rafiqzadah@gmail.com>
2023-11-24 17:24:37 -0500

    first ticket completed

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -33,6 +33,36 @@ function RenderLandingPage(props) {
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
       {/* <div className="graphs-section"> */}
+
+      <div className="graphs-section">
+        <div className="grant-rates-by-office">
+          <img
+            src={GrantRatesByOfficeImg}
+            alt="grant rates by office graph representation"
+            className="office-img"
+          />
+          <h3 className="graph-title">Search Grant Rates By Office</h3>
+        </div>
+        <div className="grant-rates-by-nationality">
+          <img
+            src={GrantRatesByNationalityImg}
+            className="nationality-img"
+            alt="grant rates by nationality graph representation"
+          />
+          <h3 className="graph-title-nationality">
+            Search Grant Rates By Nationality
+          </h3>
+        </div>
+        <div className="grant-rates-over-time">
+          <img
+            src={GrantRatesOverTimeImg}
+            className="over-time-img"
+            alt="grant rates over time graph representation"
+          />
+          <h3 className="graph-title">Search Grant Rates Over Time</h3>
+        </div>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -62,6 +92,45 @@ function RenderLandingPage(props) {
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
         {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h1>Systematic Disparity Insights</h1>
+          <div className="bottom-section-data-container">
+            <div className="bottom-section-stats">
+              <h2>36%</h2>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+            <div className="bottom-section-stats">
+              <h2>5%</h2>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+            <div className="bottom-section-stats">
+              <h2>6x Lower</h2>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="read-more-button">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/graphs')}
+          >
+            Read More
+          </Button>
+        </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
